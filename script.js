2b6fef9b7b44a243ddb69323ef0c1c8638221f31
mint5150 <musia.home@gmail.com>
2022-12-28 00:29:30 +0900

    master

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var x = 10
 var y = 5
-if (x > y) {
-console.log("xはyより大きい");
-}
-
-if (x < y) {
-  console.log("xはyより小さい");
+  if (x > y) {
+    console.log("xはyより大きい");
   }
 
-if (x >= y) {
-  console.log("xはy以上");
+  if (x < y) {
+    console.log("xはyより小さい");
   }
 
-if (x <= y) {
-  console.log("xはy以下");
+  if (x >= y) {
+    console.log("xはy以上");
+  }
+
+  if (x <= y) {
+    console.log("xはy以下");
   }
